651567e20d044023e284f5ec64c26f24dd1baa9b
Artem Sokolov <artem.sokolov.dev@gmail.com>
2024-10-30 14:17:44 -0400

    Migrations and models: * Create Category, MenuItem, Cart, Order, OrderItem models. * Create Migrations. * Add models to the administration panel.

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -1,3 +1,51 @@
 from django.db import models
+from django.contrib.auth.models import User  # noqa: F401
 
-# Create your models here.
+
+class Category(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=255, db_index=True)
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255, db_index=True)
+    price = models.DecimalField(max_digits=6, decimal_places=2, db_index=True)
+    featured = models.BooleanField(db_index=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+
+
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ("menu_item", "user")
+        # unique_together = ["menu_item", "user"]
+        # While django will accept this, specifying as a tuple instead
+        # of a list makes your code more comprehensible.
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    delivery_crew = models.ForeignKey(
+        User, on_delete=models.SET_NULL, related_name="delivery_crew", null=True
+    )
+    status = models.BooleanField(db_index=True, default=False)
+    total = models.DecimalField(max_digits=6, decimal_places=2)
+    date = models.DateField(db_index=True)
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, on_delete=models.CASCADE
+    )  # there should be a binding to the Order model here.
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField()
+    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        unique_together = ("order", "menu_item")
