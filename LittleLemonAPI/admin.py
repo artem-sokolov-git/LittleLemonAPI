05af94755452f6f7f65192a820cfda852e48b85c
Artem Sokolov <artem.sokolov.dev@gmail.com>
2024-11-01 12:16:01 -0400

    Admin Panel and Models: * Add descriptions to models * Expand admin display

--- LittleLemonAPI/admin.py
+++ LittleLemonAPI/admin.py
@@ -1,8 +1,38 @@
 from django.contrib import admin
 from .models import Category, MenuItem, Cart, Order, OrderItem
 
-admin.site.register(Category)
-admin.site.register(MenuItem)
-admin.site.register(Cart)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ("slug", "title")
+    search_fields = ("title",)
+    prepopulated_fields = {"slug": ("title",)}  # Autocomplete slug field based on title
+
+
+@admin.register(MenuItem)
+class MenuItemAdmin(admin.ModelAdmin):
+    list_display = ("title", "price", "featured", "category")
+    list_filter = ("category", "featured")
+    search_fields = ("title",)
+    ordering = ("title",)
+
+
+@admin.register(Cart)
+class CartAdmin(admin.ModelAdmin):
+    list_display = ("user", "menu_item", "quantity", "unit_price", "price")
+    list_filter = ("user",)
+    search_fields = ("user__username", "menu_item__title")
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ("user", "delivery_crew", "status", "total", "date")
+    list_filter = ("status", "date")
+    search_fields = ("user__username",)
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = ("order", "menu_item", "quantity", "unit_price", "price")
+    list_filter = ("order",)
+    search_fields = ("order__user__username", "menu_item__title")
