7fbefac073da066175dbc9cddd08a0d45c5d07b5
Artem Sokolov <artem.sokolov.dev@gmail.com>
2024-11-06 12:58:30 -0500

    Cart: * Create methods for dynamic counting in the Cart model * Create CartSerializer with validator for updating the quantity of items. * Create views for the cart. ** Clean: Clean admin for unnecessary.

--- LittleLemonAPI/models.py
+++ LittleLemonAPI/models.py
@@ -23,35 +23,42 @@ class MenuItem(models.Model):
 class Cart(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-    quantity = models.SmallIntegerField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+    quantity = models.PositiveSmallIntegerField(default=1)
+
+    @property
+    def price(self):
+        return self.quantity * self.menu_item.price
+
+    @staticmethod
+    def get_total_price(user):
+        cart_items = Cart.objects.filter(user=user)
+        return sum(item.price for item in cart_items)
 
     class Meta:
-        unique_together = ("menu_item", "user")
-        # unique_together = ["menu_item", "user"]
-        # While django will accept this, specifying as a tuple instead
-        # of a list makes your code more comprehensible.
+        # unique_together = ("menu_item", "user")
+        constraints = [
+            models.UniqueConstraint(fields=["menu_item", "user"], name="user_item")
+        ]
 
 
-class Order(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    delivery_crew = models.ForeignKey(
-        User, on_delete=models.SET_NULL, related_name="delivery_crew", null=True
-    )
-    status = models.BooleanField(db_index=True, default=False)
-    total = models.DecimalField(max_digits=6, decimal_places=2)
-    date = models.DateField(db_index=True)
+# class Order(models.Model):
+#     user = models.ForeignKey(User, on_delete=models.CASCADE)
+#     delivery_crew = models.ForeignKey(
+#         User, on_delete=models.SET_NULL, related_name="delivery_crew", null=True
+#     )
+#     status = models.BooleanField(db_index=True, default=False)
+#     total = models.DecimalField(max_digits=6, decimal_places=2)
+#     date = models.DateField(db_index=True)
 
 
-class OrderItem(models.Model):
-    order = models.ForeignKey(
-        Order, on_delete=models.CASCADE
-    )  # there should be a binding to the Order model here.
-    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
-    quantity = models.SmallIntegerField()
-    unit_price = models.DecimalField(max_digits=6, decimal_places=2)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
+# class OrderItem(models.Model):
+#     order = models.ForeignKey(
+#         Order, on_delete=models.CASCADE
+#     )  # there should be a binding to the Order model here.
+#     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+#     quantity = models.SmallIntegerField()
+#     unit_price = models.DecimalField(max_digits=6, decimal_places=2)
+#     price = models.DecimalField(max_digits=6, decimal_places=2)
 
-    class Meta:
-        unique_together = ("order", "menu_item")
+#     class Meta:
+#         unique_together = ("order", "menu_item")
--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import MenuItem
+from .models import MenuItem, Cart
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -32,3 +32,27 @@ class MenuItemSerializer(serializers.ModelSerializer):
                 "The price cannot be less than or equal to 0"
             )
         return value
+
+
+class CartSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Cart
+        fields = ["menu_item", "quantity", "price"]
+        read_only_fields = ["price"]  # Автоматически рассчитывается
+
+    def validate(self, data):
+        user = self.context["request"].user
+        menu_item = data.get("menu_item")  # Получаем товар из данных
+        quantity = data.get("quantity")  # Получаем количество
+
+        # Проверяем, есть ли товар уже в корзине у текущего пользователя
+        cart_item = Cart.objects.filter(user=user, menu_item=menu_item).first()
+
+        if cart_item:
+            # Если товар уже есть в корзине, обновляем количество
+            cart_item.quantity += quantity
+            cart_item.save()
+            # Возвращаем данные без необходимости создавать новый объект
+            raise serializers.ValidationError("Quantity updated for existing item.")
+
+        return data
