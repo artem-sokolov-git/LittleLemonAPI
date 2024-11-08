574338b0ad3950843fa002cf063e2e8f6654a13f
Artem Sokolov <artem.sokolov.dev@gmail.com>
2024-11-07 20:10:01 -0500

    Cart and Order: * Create CartSerializer, OrderSerializer * Create CartViewSet, OrderViewSet views

--- LittleLemonAPI/serializers.py
+++ LittleLemonAPI/serializers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
-from .models import MenuItem
+from .models import MenuItem, Cart, Order
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -32,3 +32,71 @@ class MenuItemSerializer(serializers.ModelSerializer):
                 "The price cannot be less than or equal to 0"
             )
         return value
+
+
+class CartSerializer(serializers.ModelSerializer):
+    title = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Cart
+        fields = (
+            "id",
+            "menu_item",
+            "title",
+            "quantity",
+        )
+
+    def get_title(self, obj):
+        return obj.menu_item.title if obj.menu_item else None
+
+    def create(self, validated_data):
+        user = self.context["request"].user
+        order, created = Order.objects.get_or_create(
+            customer=user,
+            delivery_status=False,
+        )
+        validated_data["order"] = order
+        return super().create(validated_data)
+
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_owner = serializers.CharField(source="customer.username", read_only=True)
+    items = serializers.SerializerMethodField()
+    total_price = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Order
+        fields = (
+            "order_id",
+            "order_owner",
+            "items",
+            "total_price",
+            "created_at",
+            "updated_at",
+            "deliverer",
+            "delivery_status",
+        )
+
+    def get_items(self, obj):
+        return [f"{item.title} {item.price}" for item in obj.menu_item.all()]
+
+    def get_total_price(self, obj):
+        return sum([item.price for item in obj.menu_item.all()])
+
+    def validate_deliverer(self, value):
+        if value and not value.groups.filter(name="Delivery_Crew").exists():
+            raise serializers.ValidationError(
+                "The user must be in the 'Delivery' group."
+            )
+        return value
+
+    def update(self, instance, validated_data):
+        if "customer" in validated_data:
+            instance.customer = validated_data["customer"]
+        if "deliverer" in validated_data:
+            instance.deliverer = validated_data["deliverer"]
+        instance.delivery_status = validated_data.get(
+            "delivery_status", instance.delivery_status
+        )
+        instance.save()
+        return instance
--- LittleLemonAPI/views.py
+++ LittleLemonAPI/views.py
@@ -5,11 +5,16 @@ from rest_framework.response import Response
 from rest_framework import status
 
 from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.permissions import DjangoModelPermissions
+from rest_framework.permissions import DjangoModelPermissions, IsAuthenticated
 
 
-from .models import MenuItem
-from .serializers import UserSerializer, MenuItemSerializer
+from .models import MenuItem, Cart, Order
+from .serializers import (
+    UserSerializer,
+    MenuItemSerializer,
+    CartSerializer,
+    OrderSerializer,
+)
 from .permissions import IsAdminOrManager, IsManagerOrDeliveryCrew  # noqa: F401
 
 
@@ -96,3 +101,29 @@ class MenuItemViewSet(viewsets.ModelViewSet):
     search_fields = ["category__title", "title"]
     ordering_fields = ["price"]
     permission_classes = [DjangoModelPermissions]
+
+
+class CartViewSet(viewsets.ModelViewSet):
+    queryset = Cart.objects.all()
+    serializer_class = CartSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Cart.objects.all()
+        else:
+            return Cart.objects.filter(order__customer=user)
+
+
+class OrderViewSet(viewsets.ModelViewSet):
+    queryset = Order.objects.all()
+    serializer_class = OrderSerializer
+    permission_classes = [DjangoModelPermissions]
+
+    def get_queryset(self):
+        user = self.request.user
+        if user.is_staff:
+            return Order.objects.all()
+        else:
+            return Order.objects.filter(customer=user)
